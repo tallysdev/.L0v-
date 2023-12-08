7ddae52f265dfdcb8d57c7f5f61fe733bfa05d10
Tallys-Aureliano <tallysaureliano@outlook.com>
2023-12-07 22:19:48 -0300

    :sparkles:feat: register page (#12)

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -9,9 +9,12 @@ export default function Register() {
     //     "photos": "url_to_photo1"
     //   }
     return (
-        <main className="flex flex-col items-center justify-between md:p-16 lg:p-24 pt-20 md:pt-32 text-white bg-gradient-to-tr dark:from-gray-800 dark:to-black-700">
-            <h1>Bem vindo ao Cadastro no .Lov3</h1>
-            <form action="/login" className="mt-8 grid grid-cols-2 gap-4 text-gray-900">
+        <main className="flex flex-col items-center justify-between md:p-16 lg:p-24 pt-20 md:pt-32 text-white bg-gradient-to-tr dark:from-gray-800 dark:to-black-700 flex-wrap">
+            <header>
+            <h1 className="text-3xl font-bold ">Bem vindo ao Cadastro no .Lov3</h1>
+            </header>
+            <form action="/login" >
+            <section className="mt-8 flex flex-col gap-4 text-gray-900">
                 <div className="flex justify-between items-center gap-4">
                     <label htmlFor="username" className="text-xl text-white font-bold">Nome de usuario:</label>
                     <input type="text" placeholder="Nome de Usuario" className="p-3 rounded-full text-center" />
@@ -28,7 +31,7 @@ export default function Register() {
                 </div>
 
                 <div className="flex justify-between items-center gap-4">
-                    <label htmlFor="password" className="text-xl text-white font-bold">Confirme a Senha</label>
+                    <label htmlFor="password" className="text-xl text-white font-bold">Confirme a Senha: </label>
                     <input type="password" placeholder="Confirme a Senha" className="p-3 rounded-full text-center" />
                 </div>
 
@@ -49,21 +52,17 @@ export default function Register() {
                     </div>
                 </div>
                 <div className="flex justify-between items-center gap-4">
-                    <label htmlFor="" className="text-xl text-white font-bold">Orientação Sexual: </label>
-                    <div className="flex gap-4">
-                    <input type="checkbox" name="gender" id="male" className="w-4"/>
-                    <label htmlFor="male" className="text-xl text-white ">Mulher(cis)</label>
-                    <input type="checkbox" name="gender" id="female" className="w-4"/>
-                    <label htmlFor="female" className="text-xl text-white ">Homem(cis)</label>
-                    <input type="checkbox" name="gender" id="bi" className="w-4"/>
-                    <label htmlFor="bi" className="text-xl text-white">BI</label>
-                    <input type="checkbox" name="gender" id="tqia" className="w-4"/>
-                    <label htmlFor="tqia" className="text-xl text-white ">TQIA+</label>
-                    </div>
+                    <label htmlFor="bio" className="text-xl text-white font-bold">Biografia: </label>
+                    <textarea name="bio" id="bio" cols={30} rows={5} className="p-3 rounded-xl"></textarea>
+                </div>
+                <div className="flex justify-between items-center gap-4">
+                    <label htmlFor="bio" className="text-xl text-white font-bold">Foto:</label>
+                    <input type="file" name="photos" id="photos" className=""/>
                 </div>
                 <button type="submit" className="text-white py-4 px-6 mx-auto max-w-sm h-15 w-60 text-center text-xl md:text-2xl font-bold rounded-full bg-gradient-to-tl from-pink-400 via-pink-300 to-blue-400 hover:bg-gradient-to-bl focus:ring-4 focus:ring-pink-300">
                     Cadastre-se
                 </button>
+            </section>
             </form>
         </main>
     );
