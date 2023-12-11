1ec8f7913efd02d198cb083db546058f52131dd0
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-12-11 16:02:47 -0300

    feat(route): add multer

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,16 +1,26 @@
 import { Router } from 'express';
 import { UserController } from '../controller/userController';
 import authMiddleware from '../middlewares/authMiddleware';
+import multer from 'multer';
 
 const userController = new UserController();
 
+const storage = multer.memoryStorage(); // You can customize storage options as needed
+
+// Create a multer instance with the configured storage
+const upload = multer({ storage: storage });
+
 const userRoutes = Router();
-userRoutes.post('/', userController.createUser.bind(userController));
+userRoutes.post(
+  '/',
+  userController.createUser.bind(userController),
+  upload.single('image')
+);
 userRoutes.post('/auth', userController.authUser.bind(userController));
 userRoutes.post(
   '/github/:username',
   userController.createGitHubUser.bind(userController)
-);  //TODO, revisar se realmente vale a pena manter este
+); //TODO, revisar se realmente vale a pena manter este
 userRoutes.use(authMiddleware);
 userRoutes.post(
   '/like',
