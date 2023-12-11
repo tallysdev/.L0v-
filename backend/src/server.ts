c2a59c056050f8af37c25eb865bfe29d0fe71f68
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-12-11 16:21:39 -0300

    feat(route): add multer

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,14 @@
-import express from "express";
-import { routes } from "./routes";
+import express from 'express';
+import { routes } from './routes';
+import multer from 'multer';
 
 const app = express();
+const upload = multer();
+app.use(upload.any());
+
 app.use(express.json());
 app.use(routes);
 
-const port = process.env.PORT || "3333";
+const port = process.env.PORT || '3333';
 
 app.listen(port, () => console.log(`Server is running in port ${port}`));
