90dc45125ab51a7e1bd3da495ff42c8d0d088eda
Tallys-Aureliano <tallysaureliano@outlook.com>
2023-12-14 23:25:42 -0300

    add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { routes } from './routes';
 import multer from 'multer';
+import cors from 'cors';
 
 const app = express();
 const upload = multer();
@@ -8,7 +9,9 @@ app.use(upload.single('photos'));
 
 app.use(express.json());
 app.use(routes);
-
+app.use(cors({
+    origin: '*'
+}));
 const port = process.env.PORT || '3333';
 
 app.listen(port, () => console.log(`Server is running in port ${port}`));
