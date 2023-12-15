6a2fe40881afb94c9fdcd153b3cc7f11c27088f8
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-12-15 02:02:53 -0300

    fix(server): add cors configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,9 +9,15 @@ app.use(upload.single('photos'));
 
 app.use(express.json());
 app.use(routes);
-app.use(cors({
-    origin: '*'
-}));
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
+  })
+);
 const port = process.env.PORT || '3333';
 
 app.listen(port, () => console.log(`Server is running in port ${port}`));
