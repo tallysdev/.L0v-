5736b4b6a2c3d25cffce4433341a7d999cefd969
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-12-15 07:34:57 -0300

    fix(server): add cors configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,10 +9,11 @@ app.use(upload.single('photos'));
 
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', '"Origin, X-Request-Width, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Headers', '"Origin, X-Requested-Width, Content-Type, Accept');
   next();
 });
 const port = process.env.PORT || '3333';
