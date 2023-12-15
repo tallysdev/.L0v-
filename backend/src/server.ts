1c2797f328424f45bd60f5353485dd42d1787782
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-12-15 07:48:52 -0300

    fix(server): add cors configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,11 @@ app.use(upload.single('photos'));
 
 app.use(express.json());
 app.use(routes);
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://dotlove.onrender.com',
+  })
+);
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
