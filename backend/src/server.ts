87e9e9f043b7a0e059fb4c15845026044a63f452
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-12-17 14:41:21 -0300

    chore(server): trying without cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,21 +9,7 @@ app.use(upload.single('photos'));
 
 app.use(express.json());
 app.use(routes);
-app.use(
-  cors({
-    origin: '*',
-  })
-);
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+
 const port = process.env.PORT || '3333';
 
 app.listen(port, () => console.log(`Server is running in port ${port}`));
